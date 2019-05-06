f6163b129d55d309caede2285915a9622e9673b3
mohanson <mohanson@outlook.com>
2019-05-06 14:33:46 +0800

    Fix the unused blanks

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ use chrono::Local;
 pub use log::{debug as debugln, info as println, Level};
 use log::{Log, Metadata, Record};
 
+/// None for prefix
+pub const LNONE: u8 = 0b0000_0000;
 /// The date in the local time zone: %Y-%m-%d %H:%M:%S
 pub const LTIME: u8 = 0b0000_0001;
 /// Full module path
@@ -22,11 +24,13 @@ impl Log for Rog {
             let mut prefix = String::new();
             if self.fmt & LTIME != 0 {
                 prefix.push_str(format!("{}", Local::now().format("%Y-%m-%d %H:%M:%S")).as_str());
+                prefix.push_str(" ");
             }
             if self.fmt & LPATH != 0 {
                 prefix.push_str(&record.module_path().unwrap_or_default());
+                prefix.push_str(" ");
             }
-            std::println!("{} {}", prefix, record.args());
+            std::println!("{}{}", prefix, record.args());
         }
     }
 
